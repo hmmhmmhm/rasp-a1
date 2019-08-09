2fbc84534f165c71d771aa783e75c953104c0846
hmmhmmhm <hmmhmmhm@naver.com>
2019-08-09 13:36:31 +0900

    Add LogicBase

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,34 @@
-import { init } from 'raspi'
-import { DigitalInput, DigitalOutput } from 'raspi-gpio'
+import * as Raspi from 'raspi'
+import * as GPIO from 'raspi-gpio'
 import FolderLogger from 'folder-logger'
 
-const Logger = new FolderLogger(`${process.cwd}/logs`)
+const Logger = new FolderLogger(`./logs`)
 
 Logger.debug(`Raspberry Pi Controller has been started.`)
-Logger.debug(`Controller Intailizing...`)
-init(() => {
-    Logger.debug(`Controller Intailized.`)
-    Logger.debug(``)
 
-    Logger.debug(`User Code Calling...`)
-    const input = new DigitalInput('P1-3')
-    const output = new DigitalOutput('P1-5')
-    output.write(input.read())
-    Logger.debug(`User Code Called...`)
+Logger.debug(``)
+Logger.debug(`Controller Intailizing...`)
+
+Raspi.init(async () => {
+    Logger.debug(`Controller Intailized.`)
+
+    Logger.debug(``)
+    Logger.debug(`User Code has been started...`)
+    await LogicBase()
 })
+
+const LogicBase = async () => {
+    const output = new GPIO.DigitalOutput(16)
+
+    let loopCount = 1
+    let intervalHandle = setInterval(()=>{
+        if(loopCount %2 == 1){
+            output.write(GPIO.LOW)
+            Logger.debug(`[SIGNAL] LOW (IDX: ${loopCount})`)
+        }else{
+            output.write(GPIO.HIGH)
+            Logger.debug(`[SIGNAL] HIGH (IDX: ${loopCount})`)
+        }
+        if(++loopCount == 20) clearInterval(intervalHandle)
+    }, 1000)
+}
