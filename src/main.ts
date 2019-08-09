d4849970750ca8de4bf875e0d2f0ce0a901510c6
hmmhmmhm <hmmhmmhm@naver.com>
2019-08-09 12:36:22 +0900

    Add Controller Logic

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { init } from 'raspi'
 import { DigitalInput, DigitalOutput } from 'raspi-gpio'
+import FolderLogger from 'folder-logger'
 
+const Logger = new FolderLogger(`${process.cwd}/logs`)
+
+Logger.debug(`🚧  Raspberry Pi Controller has been started.`)
+Logger.debug(`🚧  Controller Intailizing...`)
 init(() => {
+    Logger.debug(`🚧  Controller Intailized.`)
+    Logger.debug(``)
+
+    Logger.debug(`🚧  User Code Calling...`)
     const input = new DigitalInput('P1-3')
     const output = new DigitalOutput('P1-5')
     output.write(input.read())
+    Logger.debug(`🚧  User Code Called...`)
 })
